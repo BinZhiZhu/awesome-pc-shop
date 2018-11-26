7947741b384ea4093e31862025f763dae3150d75
bin <binzhizhu@gmail.com>
2018-11-27 00:59:24 +0800

    封装判断表是否存在的方法

--- common/Helper.php
+++ common/Helper.php
@@ -1,7 +1,9 @@
 <?php
 
-namespace common;
+namespace app\common;
 
+use app\models\DevUsers;
+use Yii;
 
 class Helper
 {
@@ -18,4 +20,37 @@ class Helper
         return $url;
 
     }
+
+    public static function deep_array($value, $array)
+    {
+        foreach ($array as $item) {
+            if (!is_array($item)) {
+                if ($item == $value) {
+                    return true;
+                } else {
+                    continue;
+                }
+            }
+
+            if (in_array($value, $item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static function tableExists($table_name)
+    {
+        $table_name = trim($table_name);
+        $check = Yii::$app->db->createCommand("show tables ")->queryAll();
+        //判断是否存在值是否存在二维数组中
+        $res = self::deep_array($table_name, $check);
+        if ($res) {
+            return true;
+        }
+
+        return false;
+
+    }
+
 }
