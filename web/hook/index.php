6813df8f0cd72de189780c62f5c3a30afe425d09
bin <binzhizhu@gmail.com>
2019-01-17 21:26:22 +0800

    todo

--- web/hook/index.php
+++ web/hook/index.php
@@ -1,9 +1,10 @@
 <?php
 // todo 验证、日志解析push内容并操作
 
+
 // 获取push数据内容的方法
 $requestBody = file_get_contents("php://input");
 
 // 只需这一行代码便可拉取
-shell_exec("cd /home/wwwroot/com.admin.demo && git pull"); // 目录换成项目的目录
+shell_exec("cd / && cd /home/wwwroot/com.admin.demo && git pull"); // 目录换成项目的目录
 
