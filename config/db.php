b12c9612495d7823488770f8159f64e48728e089
bin <854767470@qq.com>
2019-10-13 11:56:39 +0800

    feat(init): 迁移代码

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=demo',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=demo',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '123456',
     'charset' => 'utf8',
     'tablePrefix' => 'dev_',
 
